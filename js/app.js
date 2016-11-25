85bc5bc0deaf25b65309d09b5753ffd898adb79c
Kyle Bedell <kyle.bedell@gmail.com>
2016-11-25 15:14:14 -0500

    Buttons work on list of users

--- js/app.js
+++ js/app.js
@@ -69,5 +69,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/users');
+  $urlRouterProvider.otherwise('/app/line');
 });
